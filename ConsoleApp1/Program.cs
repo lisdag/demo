c42d03c54494d58036a0dac70013f8534efc8e1b
lisdag <1434036603@qq.com>
2023-10-05 01:22:34 +0800

    f

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ internal class asd1
     int b = 3;
     int c = 4;
     int s = 4;
-    int y = 90;
+    int y = 19;
     public static void sf()
     {
         Task hj=new Task(new Action(() => { Console.WriteLine("45678"); }));
