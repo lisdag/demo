912cf67d81166118a9f5fb6810fc1b59935a6e8f
lisdag <1434036603@qq.com>
2023-10-04 23:38:22 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class asd1
+{
+    [DllImport("Dll2.dll")]
+    public static extern int asd();
+
+    public static void sf()
+    {
+        Task hj=new Task(new Action(() => { Console.WriteLine("45678"); }));
+        hj.Start();
+       
+    }
+}
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        int a = 42;
+            int b = 119;
+            int c = a + b;
+        Thread t=new Thread(new ThreadStart(delegate(){ Console.WriteLine(c+"123");}));
+        t.Start();
+        asd1 hj=new asd1();
+        Console.Write(asd1.asd() + "\n");
+        Console.Read();
+        }
+    }
+
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#pragma comment(lib,"StaticLib1.lib")
+#include <iostream>
+#include"../StaticLib1/pch.h"
+
+int main()
+{
+    asd a;
+    std::cout << a.test();
+}
+
