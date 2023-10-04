fce045abcfc40b5ae943ba6e5f7f354850e09fac
lisdag <1434036603@qq.com>
2023-10-05 00:59:41 +0800

    as

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ internal class asd1
     [DllImport("Dll2.dll")]
     public static extern int asd();
     int a=0;
-    int b = 1;
+    int b = 3;
     public static void sf()
     {
         Task hj=new Task(new Action(() => { Console.WriteLine("45678"); }));
